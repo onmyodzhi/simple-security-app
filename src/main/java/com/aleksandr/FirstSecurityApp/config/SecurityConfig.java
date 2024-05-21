1a62211879cf385d70c8daa1447064e16bceae1a
onmyodzhi <mrkobalt18@gmail.com>
2024-05-21 15:21:32 +0300

    created code to help make custom authentication

--- src/main/java/com/aleksandr/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/aleksandr/FirstSecurityApp/config/SecurityConfig.java
@@ -1,19 +1,32 @@
 package com.aleksandr.FirstSecurityApp.config;
 
-import com.aleksandr.FirstSecurityApp.security.AuthProviderImpl;
+import com.aleksandr.FirstSecurityApp.services.PeopleDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    /*
+    Code in comment will use only when you need to make default authentication
+     */
+    //private final AuthProviderImpl authProvider;
+    private final PeopleDetailsService peopleDetailsService;
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        //auth.authenticationProvider(authProvider);
+        auth.userDetailsService(peopleDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
