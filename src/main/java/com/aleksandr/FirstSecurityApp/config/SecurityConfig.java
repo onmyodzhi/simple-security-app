f0a9e0d4a73484d366ce50ffdd8eb2ebb096881d
onmyodzhi <mrkobalt18@gmail.com>
2024-05-22 09:51:25 +0300

    added custom authentication page

--- src/main/java/com/aleksandr/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/aleksandr/FirstSecurityApp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,12 +15,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PeopleDetailsService peopleDetailsService;
     /*
     Code in comment will use only when you need to make default authentication
      */
     //private final AuthProviderImpl authProvider;
-    private final PeopleDetailsService peopleDetailsService;
 
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //auth.authenticationProvider(authProvider);
         auth.userDetailsService(peopleDetailsService);
